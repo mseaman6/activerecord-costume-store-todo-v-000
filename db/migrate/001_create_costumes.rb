148623348246ef4d78eb384bf8c53bf1da66f251
Monica Seaman <monica.l.seaman@gmail.com>
2018-07-11 16:46:57 +0000

    automatically backed up by learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -3,6 +3,9 @@ class CreateCostumes < ActiveRecord::Migration[4.2]
   def change
     create_table :costumes do |t|
       t.string :name
+      t.integer :price
+      t.ingteger :size
+      t.string :image_url
 
     end
   end
